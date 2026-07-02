c6e5eda8d514437549d4ca8620cf10631e573560
ALEX HUNTER <alexhuntercct@gmail.com>
2026-07-02 21:49:28 +0600

    Create Block.py

---
+++ Block.py
@@ -0,0 +1,10 @@
+import protected
+import inspect
+
+try:
+    print("Auto_run arguments:", inspect.signature(Tarmuxset.auto_run))
+except:
+    print("Signature not found. Trying main...")
+
+# যদি main কাজ করে তবে এটিই ব্যবহার করুন
+protected.main()
