39df63dade246f65f3c2809d21ec8e228351d317
ALEX HUNTER <alexhuntercct@gmail.com>
2026-07-02 23:25:04 +0600

    Update Block.py

--- Block.py
+++ Block.py
@@ -1,4 +1,4 @@
-import protected
+import Security
 import inspect
 
 try:
@@ -7,4 +7,4 @@ except:
     print("Signature not found. Trying main...")
 
 # যদি main কাজ করে তবে এটিই ব্যবহার করুন
-protected.main()
+Security.main()
